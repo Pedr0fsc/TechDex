2c00e0024b0754efbf15ff6873921e7e6c8ab38c
Pedro da Fonseca Schwertner <pedrodafonsecaschwertner@gmail.com>
2025-11-10 10:02:36 -0300

    Encrypted password method added

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -1,15 +1,17 @@
 <?php
 include "../core/Conexao.php";
 
-function cadastrar_usuario($nome, $email, $senha) {
+function cadastrar_usuario($nome, $email, $senha)
+{
     global $conection;
+
+    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
     $stmt = mysqli_stmt_init($conection);
     $sql = "INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)";
     mysqli_stmt_prepare($stmt, $sql);
-    mysqli_stmt_bind_param($stmt, "sss", $nome, $email, $senha);
+    mysqli_stmt_bind_param($stmt, "sss", $nome, $email, $senha_hash);
     $resultado = mysqli_stmt_execute($stmt);
 
     return $resultado;
 }
-
